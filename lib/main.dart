6b9db4f9168b177394753bb8921a2ba2a1a02dc4
progMaro <marwan01125211013@gmail.com>
2024-10-15 18:23:11 +0300

    change theme and make intial architecture of the docs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/home/presentation/views/home_view.dart';
+
 main() {
   runApp(const NotesApp());
 }
@@ -9,6 +11,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      home: const NotesView(),
+    );
   }
 }
